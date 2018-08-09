e701ba2bd38ede4e6b9890eb978925eb3c1710a2
Davinna <dav1nna@me.com>
2018-08-09 15:15:18 -0500

    running test code.

--- amazonSellerMatchLowPrice.js
+++ amazonSellerMatchLowPrice.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         amazoneSellerMatchLowPrice
 // @namespace    https://greasyfork.org/users/98044
-// @version      0.1
+// @version      0.2
 // @description  Will refresh "Manage  Inventory" page every 16mins, click all "Match Low Price" buttons, then "Save"
 // @author       Davinna
 // @icon         https://turkerhub.com/data/avatars/l/0/594.jpg?1485199123
@@ -10,14 +10,13 @@
 // @license      https://opensource.org/licenses/MIT
 // @copyright    2017-2018 Davinna
 // ==/UserScript==
-
 $(document).ready(function(){
   'use strict';
   const $matchLowPrice = $('button span.a-dropdown-prompt:visible');
   const $saveAll = $('span.a-declarative span.a-button-inner a.a-button-text:visible');
-
+   
   $matchLowPrice.click();
-  setTimeout(function(){$saveAll.click()}, 5000);
+  setTimeout(function(){$saveAll.click()}, 2000);
   setInterval(function(){location.reload()}, 960000);
 
 });
