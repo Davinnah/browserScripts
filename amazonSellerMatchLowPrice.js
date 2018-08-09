39fa383879399638d0943d08bd551d144a95192f
Davinna <dav1nna@me.com>
2018-08-09 08:20:43 -0500

    added amazonSellerMatchLowPrice.js

---
+++ amazonSellerMatchLowPrice.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         amazoneSellerMatchLowPrice
+// @namespace    TODO
+// @version      0.1
+// @description  Will refresh "Manage  Inventory" page every 16mins, click all "Match Low Price" buttons, then "Save"
+// @author       Davinna
+// @icon         https://turkerhub.com/data/avatars/l/0/594.jpg?1485199123
+// @include      *sellercentral.amazon.com/inventory?viewId=PRICING*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @license      https://opensource.org/licenses/MIT
+// @copyright    2017-2018 Davinna
+// ==/UserScript==
