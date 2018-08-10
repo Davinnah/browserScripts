28533b369ca36d01739c5703e9f583cd1b63ba47
Davinna <dav1nna@me.com>
2018-08-10 06:28:21 -0500

    starting new strategy. looking for method to determine page is fully loaded.

--- amazonSellerMatchLowPrice.js
+++ amazonSellerMatchLowPrice.js
@@ -1,32 +1,8 @@
-// ==UserScript==
-// @name         amazoneSellerMatchLowPrice
-// @namespace    https://greasyfork.org/users/98044
-// @version      0.6
-// @description  Will refresh "Manage  Inventory" page every 16mins, click all "Match Low Price" buttons, then "Save"
-// @author       Davinna
-// @icon         https://turkerhub.com/data/avatars/l/0/594.jpg?1485199123
-// @include      *sellercentral.amazon.com/inventory?viewId=PRICING*
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @license      https://opensource.org/licenses/MIT
-// @copyright    2017-2018 Davinna
-// ==/UserScript==
-
 $(document).ready(function(){
   'use strict';
 
-  const $matchLowPriceButtons = $('button span.a-dropdown-prompt:visible');
-  const $saveAllButton = $('span.a-declarative span.a-button-inner a.a-button-text:visible');
-  console.log($matchLowPriceButtons, $saveAllButton);
+  document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+    alert("DOM fully loaded and parsed");
 
-  setTimeout(function () {
-    $matchLowPriceButtons.click(),
-    60000
-  });
-
-  setTimeout(function () {
-    $matchLowPriceButtons.click(),
-    60000
-  });
-  setInterval(function(){location.reload()}, 960000);
-
-});
+}
