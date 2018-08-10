2b828ca2886f9bd9dcb0292926704ed97c4c06fb
Davinna <dav1nna@me.com>
2018-08-10 08:29:22 -0500

    modified function

--- amazonSellerMatchLowPrice.js
+++ amazonSellerMatchLowPrice.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         amazoneSellerMatchLowPrice
 // @namespace    https://greasyfork.org/users/98044
-// @version      0.8
-// @description  Will refresh "Manage  Inventory" page every 16mins, click all "Match Low Price" buttons, then "Save"
+// @version      0.10
+// @description  Will refresh "Manage  Inventory" page every 16mins, click all "Match Low Price" buttons,
+// @description  then all the "Save" buttons.
 // @author       Davinna
 // @icon         https://turkerhub.com/data/avatars/l/0/594.jpg?1485199123
 // @include      *sellercentral.amazon.com/inventory?viewId=PRICING*
@@ -13,18 +14,21 @@
 $(document).ready(function(){
   'use strict';
 
-  setInterval(function(){matchLowPrice(),alert('clicked')}, 5000);
-
+  setInterval(function(){matchLowPrice()}, 60000);
+  setInterval(function(){local.reload()}, 300000);
 
 });
 
 function matchLowPrice() {
 
-  let $matchLowPriceButtons = $('button span.a-dropdown-prompt:visible');
+  const $matchLowPriceButtons = $('button span.a-dropdown-prompt:visible');
   console.log($matchLowPriceButtons);
 
   for (var i = 0; i < $matchLowPriceButtons.length; i++) {
-    $matchLowPriceButtons[i].click
+    $matchLowPriceButtons[i].click();
+    if(i===$matchLowPriceButtons.length-1) console.log('clicked all');
   }
-
+  const $saveAllButton = $('span.a-declarative span.a-button-inner a.a-button-text:visible');
+  $saveAllButton.click();
+  console.log('clicked save');
 }
